71a731d4d85267c9998b33a3e69d0a18a664bcc5
Gennadiy Rusetskiy <g_rusetskiy@wargaming.net>
2022-08-20 17:25:20 +0300

    Fixed css sourceMap

--- build-scripts/rollup.config.js
+++ build-scripts/rollup.config.js
@@ -64,7 +64,7 @@ export default {
       plugins: [
         postcssPresetEnv()
       ],
-      sourceMap: true,
+      sourceMap: isDevelopment,
       minimize: !isDevelopment,
       extract: false,
       extensions: ['.sass']
